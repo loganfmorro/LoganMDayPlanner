b600ffc2845dc7dc46bca778cab94320a519930e
Logan Morro <loganfmorro@gmail.com>
2020-10-16 20:00:35 -0400

    had to debug some code as it was no longer working

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    const test = false;
+    const t = false;
     const now = moment().format('MMMM Do YYYY');
     let nowHour24 = moment().format('H');
     let nowHour12 = moment().format('h');
   
-    if (test) {
+    if (t) {
       nowHour24 = 13;
       nowHour12 = 1;
     }
@@ -14,22 +14,22 @@ $(document).ready(function() {
 
     //Storing and Saving Daily Plans
     const saveIcon = "./images/save-regular.svg"; 
-    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
-    if (test) { console.log(storedPlans); }
+    let userPlans = JSON.parse(localStorage.getItem("userPlans"));
+    if (t) { console.log(userPlans); }
   
-    if (storedPlans !== null) {
-      planTextArr = storedPlans;
+    if (userPlans !== null) {
+      planTextArr = userPlans;
     } else {
       planTextArr = new Array(9);
       planTextArr[4] = "Take a Lunch Break!"; //example text to show user to type
     }
   
-    if (test) { console.log("full array of planner text",planTextArr); }
+    if (t) { console.log("full array of planner text",planTextArr); }
     let $plannerDiv = $('#plannerContainer');
     $plannerDiv.empty();
     
     //Setting the time on the left side of the planner
-    if (test) { console.log("current time",nowHour12); }
+    if (t) { console.log("current time",nowHour12); }
     for (let hour = 9; hour <= 17; hour++) {
       let index = hour - 9;
       
@@ -46,21 +46,21 @@ $(document).ready(function() {
       $timeBoxSpn.attr('class','timeBox');
       
       //If function to set AM and PM variables
-      let displayHour = 0;
+      let hourAMPM = 0;
       let ampm = "";
       if (hour >= 12) { 
-        displayHour = hour - 12;
-        if (displayHour === 0){
-            displayHour = 12
+        hourAMPM = hour - 12;
+        if (hourAMPM === 0){
+            hourAMPM = 12
         }
         ampm = "pm";
       } else {
-        displayHour = hour;
+        hourAMPM = hour;
         ampm = "am";
       }
       
       //Time portion of left side
-      $timeBoxSpn.text(`${displayHour} ${ampm}`);
+      $timeBoxSpn.text(`${hourAMPM} ${ampm}`);
       $rowDiv.append($col2TimeDiv);
       $col2TimeDiv.append($timeBoxSpn);
 
@@ -87,19 +87,19 @@ $(document).ready(function() {
       $col1SaveDiv.append($saveBtn);
   
       // Color coding the rows based on the current time
-      updateRowColor($rowDiv, hour);
+      rowTimeColor($rowDiv, hour);
       $plannerDiv.append($rowDiv);
     };
-    function updateRowColor ($hourRow,hour) { 
-      if (test) { console.log("rowColor ",nowHour24, hour); }
+    function rowTimeColor ($hourRow,hour) { 
+      if (t) { console.log("rowColor ",nowHour24, hour); }
       if ( hour < nowHour24) {
-        if (test) { console.log("lessThan"); }
+        if (t) { console.log("lessThan"); }
         $hourRow.css("background-color","lightgrey")
       } else if ( hour > nowHour24) {
-        if (test) { console.log("greaterthan"); }
+        if (t) { console.log("greaterthan"); }
         $hourRow.css("background-color","lightgreen")
       } else {
-        if (test) { console.log("equal"); }
+        if (t) { console.log("equal"); }
         $hourRow.css("background-color","violet")
       }
     };
@@ -108,23 +108,23 @@ $(document).ready(function() {
     $(document).on('click','i', function(event) {
       event.preventDefault();  
   
-      if (test) { console.log('click pta before '+ planTextArr); }
+      if (t) { console.log('click pta before '+ planTextArr); }
       let $index = $(this).attr('save-id');
       let inputId = '#input-'+$index;
       let $value = $(inputId).val();
       planTextArr[$index] = $value;
-      if (test) { console.log('value ', $value); }
-      if (test) { console.log('index ', $index); }
-      if (test) { console.log('click pta after '+ planTextArr); }
+      if (t) { console.log('value ', $value); }
+      if (t) { console.log('index ', $index); }
+      if (t) { console.log('click pta after '+ planTextArr); }
       $(`#saveid-${$index}`).removeClass('shadowPulse');
-      localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+      localStorage.setItem("userPlans", JSON.stringify(planTextArr));
     });  
     
     // If the user changes their input, this function will save the color change
     $(document).on('change','input', function(event) {
       event.preventDefault();  
-      if (test) { console.log('onChange'); }
-      if (test) { console.log('id', $(this).attr('hour-index')); }
+      if (t) { console.log('onChange'); }
+      if (t) { console.log('id', $(this).attr('hour-index')); }
       let i = $(this).attr('hour-index');
       $(`#saveid-${i}`).addClass('shadowPulse');
     });
